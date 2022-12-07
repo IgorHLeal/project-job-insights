069ada4fc99895ee73fb8745fea029995eadca85
Igor Leal <igorhenriqueleal3@gmail.com>
2022-12-07 09:16:38 -0300

    test: Implementando um teste para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+import pytest
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+
+
+with pytest.raises(FileNotFoundError):
+    count_ocurrences("data/java.java", "Java")
