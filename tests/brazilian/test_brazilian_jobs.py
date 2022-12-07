d5b701f2a29207e9ae06883aec4c85a244745b79
Igor Leal <igorhenriqueleal3@gmail.com>
2022-12-07 09:36:03 -0300

    test: Implementando testes para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,23 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file(path)[0] == {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
+
+    assert read_brazilian_file(path)[1] == {
+        "title": "Motorista",
+        "salary": "3000",
+        "type": "full time",
+    }
+
+    assert read_brazilian_file(path)[2] == {
+        "title": "Analista de Software",
+        "salary": "4000",
+        "type": "full time",
+    }
